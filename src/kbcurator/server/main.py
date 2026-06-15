8af4c1aae3676caee07762e1e50d470b0dec68f2
aabhi3 <abhishek.3.mishra@coforge.com>
2026-06-15 17:16:36 +0530

    Update main.py
    frame-ancestors

--- src/kbcurator/server/main.py
+++ src/kbcurator/server/main.py
@@ -160,7 +160,7 @@ class SecurityAndCORSMiddleware(BaseHTTPMiddleware):
             "font-src 'self'; "
             "img-src 'self' data: https:; "
             "object-src 'none'; "
-            "frame-ancestors 'self';"
+            "frame-ancestors 'self' https://login.microsoftonline.com https://*.login.microsoftonline.com;"
         )
         # HTTP Strict Transport Security
         response.headers["Strict-Transport-Security"] = (
